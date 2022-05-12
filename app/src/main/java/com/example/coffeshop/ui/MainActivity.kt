34953e7ce318846c9a3357313f26462ae64d9058
mehraj <mehraj@cdse-it.com>
2022-05-12 10:00:36 +0530

    Added coffee items view and about view

--- app/src/main/java/com/example/coffeshop/ui/MainActivity.kt
+++ app/src/main/java/com/example/coffeshop/ui/MainActivity.kt
@@ -1,15 +1,58 @@
 package com.example.coffeshop.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.coffeshop.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(){
+    lateinit var bottomBar:BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setCurrentFragment(FormFragment())
+        bottomBar=findViewById(R.id.bottomNavigationView)
+
+           bottomBar.setOnItemSelectedListener { item ->
+               if(item.itemId==R.id.form)
+               {
+
+                   setCurrentFragment(FormFragment())
+                  // return  true;
+               }
+
+               if(item.itemId==R.id.coffeeItems)
+               {
+
+                   setCurrentFragment(CoffeItems())
+                  // return  true;
+               }
+
+               if(item.itemId==R.id.about)
+               {
+
+                   setCurrentFragment(AboutFragment())
+                  // return  true;
+               }
+
+
+               if(item.itemId==R.id.camera)
+               {
+
+                   setCurrentFragment(CameraFragment())
+                  // return  true;
+               }
+
+
+             true;
+           }
+
+
     }
 
 
@@ -18,4 +61,6 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.flFragment,fragment)
             commit()
         }
+
+
 }
